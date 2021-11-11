53d64a84c23338d36a3e41eecf54b685f3d8ecf9
ale2360 <78240010+ale2360@users.noreply.github.com>
2021-11-11 00:34:10 -0600

    Create Operaciones.js

---
+++ js/Operaciones.js
@@ -0,0 +1,27 @@
+function opera()
+{
+var num1=Number(document.getElementById('n1').value),
+   
+
+    suma = document.getElementById("suma");
+
+
+var suma= Math.sqrt(num1);
+// div=num1/num2,
+// res=num1-num2,
+// mult=num1*num2;
+
+document.getElementById('suma').value=suma;
+
+
+// document.getElementById('div').value=div;
+
+// document.getElementById('res').value=res;
+
+// document.getElementById('mult').value=mult;
+
+
+
+
+
+}
